5dfc389ebcf7618174b7e9ae76bc6f315598cea9
Калинин Николай <99505820+KeyK4@users.noreply.github.com>
2024-04-07 04:57:55 +0400

    add NUnit Console

--- tpo_lab2/Tests/obj/Debug/net7.0/Tests.AssemblyInfo.cs
+++ tpo_lab2/Tests/obj/Debug/net7.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1648b5f9c2fd5efa1bfa2ef643a57544a6b8d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3a1326970a63be727e550b537e85e64b1db14e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
